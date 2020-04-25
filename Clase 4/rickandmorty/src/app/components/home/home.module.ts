5e63f3402f06c393a79ac507a2f5582129f50e9f
Cristian Ortega <c.ortegamu@gmail.com>
2020-04-25 18:39:57 -0400

    avance proyecto api

--- Clase 4/rickandmorty/src/app/components/home/home.module.ts
+++ Clase 4/rickandmorty/src/app/components/home/home.module.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { BannerComponent } from './components/banner/banner.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    BannerComponent
   ],
   imports: [
     CommonModule,
--- Clase 4/rickandmorty/src/app/shared/shared.module.ts
+++ Clase 4/rickandmorty/src/app/shared/shared.module.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '../components/material/material.module';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     MaterialModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
