074bf8b972100c03a997d6b5a360adaf534aad2c
Cristian Ortega <c.ortegamu@gmail.com>
2020-05-05 16:37:35 -0400

    avance del curso

--- Clase 5/firebase-app/src/app/app.module.ts
+++ Clase 5/firebase-app/src/app/app.module.ts
@@ -1,62 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule, StorageBucket } from '@angular/fire/storage';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AdminComponent } from './components/admin/admin.component';
-import { ProfileComponent } from './components/admin/profile/profile.component';
-import { LoginComponent } from './components/auth/login/login.component';
-import { ModalComponent } from './shared/components/modal/modal.component';
-import { TableComponent } from './shared/components/table/table.component';
-import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
-import { AboutComponent } from './components/pages/about/about.component';
 import { ContainerAppComponent } from './components/pages/container-app/container-app.component';
-import { HomeComponent } from './components/pages/home/home.component';
 import { DetailsPostComponent } from './components/posts/details-post/details-post.component';
 import { EditPostComponent } from './components/posts/edit-post/edit-post.component';
-import { ListPostComponent } from './components/posts/list-post/list-post.component';
+import { EditPostModule } from './components/posts/edit-post/edit-post.module';
 import { NewPostComponent } from './components/posts/new-post/new-post.component';
-import { AdminModule } from './components/admin/admin.module';
-import { ProfileModule } from './components/admin/profile/profile.module';
-import { LoginModule } from './components/auth/login/login.module';
-import { AboutModule } from './components/pages/about/about.module';
-import { HomeModule } from './components/pages/home/home.module';
-import { ListPostModule } from './components/posts/list-post/list-post.module';
 import { NewPostModule } from './components/posts/new-post/new-post.module';
-import { PostComponent } from './components/post/post.component';
+import { MaterialModule } from './material.module';
+import { ModalComponent } from './shared/components/modal/modal.component';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent,
-    ProfileComponent,
-    LoginComponent,
     ModalComponent,
-    TableComponent,
     ToolbarComponent,
-    AboutComponent,
     ContainerAppComponent,
-    HomeComponent,
     DetailsPostComponent,
     EditPostComponent,
-    ListPostComponent,
-    NewPostComponent,
-    PostComponent
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    AdminModule,
-    ProfileModule,
-    LoginModule,
-    AboutModule,
-    HomeModule,
-    ListPostModule,
-    NewPostModule
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    AppRoutingModule,
+    NewPostModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    EditPostModule
   ],
-  providers: [],
+  entryComponents: [ModalComponent],
+  providers: [
+    { provide: StorageBucket, useValue: 'gs://fir-app-b184d.appspot.com/' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Clase 5/firebase-app/src/environments/environment.ts
+++ Clase 5/firebase-app/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBb2zC3zQwU-IeCWQJ4gVE08TqH2A7mFf0',
+    authDomain: 'fir-app-b184d.firebaseapp.com',
+    databaseURL: 'https://fir-app-b184d.firebaseio.com',
+    projectId: 'fir-app-b184d',
+    storageBucket: 'fir-app-b184d.appspot.com',
+    messagingSenderId: '311810976792',
+    appId: '1:311810976792:web:2cda09c669fe13689ae76d'
+  }
 };
 
 /*
